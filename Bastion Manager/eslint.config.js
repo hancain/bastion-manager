642487e8ab8af4360312dde46ddaa4fe02f72e55
Hannah Cain <hcsch18@gmail.com>
2025-11-06 21:16:04 -0500

    Update to vuetify

--- Bastion Manager/eslint.config.js
+++ Bastion Manager/eslint.config.js
@@ -1,17 +1,3 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+import vuetify from 'eslint-config-vuetify'
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-]
+export default vuetify()
